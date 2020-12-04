9cad0efc51cba69a2295e10cda02b5489905fd8e
Sascha Meiers <sascha.meiers.embl@gmail.com>
2020-12-04 23:16:45 +0100

    My first RUST program  :heart_eyes: * solves puzzle 01 of adventofcode2020 * incl. build + debug rule for VS Code

---
+++ advent01/src/main.rs
@@ -0,0 +1,37 @@
+// use std::env;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+// From: https://doc.rust-lang.org/rust-by-example/std_misc/file/read_lines.html
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+
+fn main() {
+
+    let filename = "data/advent01.txt";
+    let mut vec = Vec::new();
+
+    if let Ok(lines) = read_lines(filename) {
+        for line in lines {
+            // these commands may "panick"
+            let string: String = line.unwrap();
+            if string.trim().len() == 0 { continue; }
+            let num: u32 = string.trim().parse::<u32>().unwrap();
+            vec.push(num);
+        }
+    }
+
+    for a in 0..vec.len() {
+        for b in 0..vec.len() {
+            if a<=b { continue; }
+            if vec[a] + vec[b] == 2020 {
+                println!("{0} + {1} = {2} --> {0} * {1} = {3}", vec[a], vec[b], vec[a]+vec[b], vec[a]*vec[b]);
+            }
+        }
+    }
+}
